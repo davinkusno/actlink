7a33bc9cc6afc3c2324e139736e55e60e64b91e8
Davin <davinkusno@gmail.com>
2025-01-18 20:02:46 +0700

    Update bootstrap cache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -20,5 +20,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->create()
-    ->useCachePath(env('APP_CACHE_PATH', base_path('bootstrap/cache')));
+    ->create();
+
+// Dynamically override cache path
+$app->bind('path.cache', function () {
+    return env('APP_CACHE_PATH', base_path('bootstrap/cache'));
+});
+
+return $app;
