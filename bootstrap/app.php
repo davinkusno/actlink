bbb28fdc40c51224cb3e98ba566ae1cc41466145
Davin <davinkusno@gmail.com>
2025-01-18 19:58:41 +0700

    Add bootstrap cache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,4 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create()
+    ->useCachePath(env('APP_CACHE_PATH', base_path('bootstrap/cache')));
